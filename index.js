a0c448b7de26a5619f5ca4e160e835bb1fede9ea
Lakshmi-Shravani-K-24 <20becs077.lakshmishravani@gmail.com>
2024-02-25 12:48:50 +0530

    mongodb test using memory server and 100 percent coverage

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log('This is the starting point');
+const Battery = require('./BatterySchema');
+
+async function createBattery(batteryData) {
+  try {
+    const newBattery = await Battery.create(batteryData);
+    return newBattery;
+  } catch (error) {
+    throw new Error(`Failed to create battery: ${error.message}`);
+  }
+}
+
+async function findBatteryById(batteryId) {
+  try {
+    const battery = await Battery.findOne({batteryId});
+    return battery;
+  } catch (error) {
+    throw new Error(`Failed to find battery: ${error.message}`);
+  }
+}
+
+async function updateBattery(batteryId, updateData) {
+  try {
+    const updatedBattery = await Battery.findOneAndUpdate({batteryId}, updateData, {new: true});
+    return updatedBattery;
+  } catch (error) {
+    throw new Error(`Failed to update battery: ${error.message}`);
+  }
+}
+
+async function deleteBattery(batteryId) {
+  try {
+    await Battery.deleteOne({batteryId});
+  } catch (error) {
+    throw new Error(`Failed to delete battery: ${error.message}`);
+  }
+}
+
+module.exports = {createBattery, findBatteryById, updateBattery, deleteBattery};
